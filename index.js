d6f0df6e81a5fe3dbc980ef453efb7bbbb7647b4
Esmael Sabir <esmaelsabir9@gmail.com>
2025-02-01 21:09:15 +0000

    some requerment updates

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const multer = require('multer'); // Import multer for file uploads
-const path = require('path');
-require('dotenv').config();
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+require('dotenv').config()
 
-const app = express();
+var app = express();
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+app.use((req, res, next) => {
+  console.log("method: " + req.method + "  |  path: " + req.path + "  |  IP - " + req.ip);
+  next();
+});
+
+
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Set up multer for file uploads
-const upload = multer({ dest: 'uploads/' }); // Files will be temporarily stored in the 'uploads/' folder
-
-// POST endpoint for file uploads
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: 'No file uploaded' });
+  console.log(req.file);
+  if (req.file) {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  } else {
+    res.json("No file found");
   }
-
-  // Extract file metadata
-  const { originalname: name, mimetype: type, size } = req.file;
-
-  // Return the file metadata in the JSON response
-  res.json({
-    'name':name,
-    'type':type,
-    'size':size,
-  });
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port);
+  console.log('Your app is listening on port ' + port)
 });
